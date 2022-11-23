626bf2ac7b699945c2780ebb61d0dd558ac61c5e
easeghehey <emanuelaseghey9@gmail.com>
2022-11-23 11:35:18 -0500

    added test script

--- robotomaton/robotomaton-iterative.py
+++ robotomaton/robotomaton-iterative.py
@@ -19,8 +19,25 @@ def iter(previous, sprocket):
 
     return cache[-1]
 
+def iter_better(previous, sprocket):
+    cache = [0] * len(sprocket)
+    total = sprocket[0]
+    cache[0] = sprocket[0]
+
+    for i in range(1, len(sprocket)):
+        total = sprocket[i]
+
+        if previous[i] == i:
+            total += cache[i - 1]
+        else:
+            total += cache[i-1] - cache[i - 1 - previous[i]]
+        cache[i] = cache[i - 1] + total
+
+    return total
+
+
 if __name__ == "__main__":
     previous = [0,1,1,3]
     sprockets = [3,2,10,7]
 
-    print(iter(previous, sprockets))
+    print(iter(previous, sprockets) == iter_better(previous, sprockets))
---
+++ robotomaton/test-comparison.py
@@ -0,0 +1,75 @@
+import random
+
+# t1
+# sprocket = [3, 11, 31, 29, 5]
+# previous = [0, 0, 1, 2, 4]
+
+# t2
+# previous = [0,0,1,0,4]
+# sprocket = [3,11,31,29,5]
+
+# t3
+# previous = [0, 1, 1, 3]
+# sprocket = [3,2,10,7]
+
+# t4
+# sprocket = [random.randrange(200)]
+# previous = [0]
+#
+# for i in range(1, random.randrange(200)):
+#     sprocket.append(random.randrange(200))
+#     previous.append(random.randrange(i))
+
+
+# print(len(sprocket) == len(previous))
+# print(previous)
+
+def iter(previous, sprocket):
+    total = sprocket[0]
+    cache = [0] * len(sprocket)
+
+    cache[0] = sprocket[0]
+
+    for i in range(1, len(sprocket)):
+        total = sprocket[i]
+
+        if previous[i] == i:
+            total += cache[i - 1]
+
+        else:
+            total += cache[i - 1] - cache[i - previous[i] - 1]
+
+        cache[i] = cache[i - 1] + total
+    return total
+
+
+def robotomaton(num, cache, previous, sprockets):
+    if cache[num] != -1:
+        return cache[num]
+
+    if previous[num] == 0:
+        return sprockets[num]
+
+    cost = sprockets[num]
+    for i in range(num - 1, num - previous[num] - 1, -1):
+        cost += robotomaton(i, cache, previous, sprockets)
+
+    cache[num] = cost
+    return cache[num]
+
+
+for i in range(1, 10000):
+    sprocket = [random.randrange(200)]
+    previous = [0]
+
+    for j in range(1, random.randrange(200)):
+        sprocket.append(random.randrange(200))
+        previous.append(random.randrange(j))
+
+    cache = [-1] * len(sprocket)
+    iter_var = iter(previous, sprocket)
+    memo = robotomaton(len(sprocket) - 1, cache, previous, sprocket)
+    print('\nresult: \niter ', iter_var, '\nmemo ', memo)
+    print('Equal: ', memo == iter_var)
+    if not memo == iter_var:
+        print("wrong")
